53396bef01e1d6b666f51185a5d76c07f7fbea36
Md Jubair Hasan Miraz <92200894+jhmiraz1@users.noreply.github.com>
2022-04-20 01:25:25 +0600

    Add files via upload

---
+++ java code/javaapplication38/maxValueFromArray.java
@@ -0,0 +1,26 @@
+
+package javaapplication38;
+
+import java.util.Scanner;
+
+
+public class maxValueFromArray {
+    
+    public static void main (String[]args){
+        Scanner m1= new Scanner(System.in);
+        
+        int[]array1=new int[5];
+        
+        System.out.println("Enter array element : ");
+        
+        for (int i=0; i<5;i++)
+        {
+        array1[i]=m1.nextInt();
+        
+        }
+    
+    
+    }
+    
+    
+}
